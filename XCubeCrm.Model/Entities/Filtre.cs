9be4eee472924249fc21e1a8b263e6938c983a7c
ugur-ozcan <ugur.ozcan@outlook.com>
2024-11-28 20:31:25 +0300

    Add project files.

---
+++ XCubeCrm.Model/Entities/Filtre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using XCubeCrm.Common.Enums;
+using XCubeCrm.Model.Attributes;
+using XCubeCrm.Model.Entities.Base;
+
+namespace XCubeCrm.Model.Entities
+{
+    public class Filtre : BaseEntity
+    {
+        [Index("IX_Kod", IsUnique = false)]
+        public override string Kod { get; set; }
+        [Required, StringLength(100), ZorunluAlan("Filtre Adı", "txtFiltreAdi")]
+        public string FiltreAdi { get; set; }
+        [Required, StringLength(1000), ZorunluAlan("Filtre Adı", "txtFiltreMetni")]
+        public string FiltreMetni { get; set; }
+        [Required,]
+        public KartTuru KartTuru { get; set; }
+    }
+}
---
+++ XCubeCrm.UI.Win/Forms/UserControls/Controls/Grid/MyGridControl.cs
@@ -0,0 +1,145 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors.Mask;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid.Registrator;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCubeCrm.UI.Win.Interfaces;
+
+namespace XCubeCrm.UI.Win.Forms.UserControls.Controls.Grid
+{
+    [ToolboxItem(true)]
+    public class MyGridControl : GridControl
+    {
+        protected override BaseView CreateDefaultView()
+        {
+            var view = (GridView)CreateView("MyGridView");
+
+            
+            view.Appearance.ViewCaption.ForeColor = Color.Maroon;
+            view.Appearance.HeaderPanel.ForeColor = Color.Maroon;
+            view.Appearance.HeaderPanel.TextOptions.HAlignment = HorzAlignment.Center;
+            view.Appearance.FooterPanel.ForeColor = Color.Maroon;
+            view.Appearance.FooterPanel.Font = new Font(new FontFamily("Tahoma"), 8.25f, FontStyle.Bold);
+
+            view.OptionsMenu.EnableColumnMenu = false;
+            view.OptionsMenu.EnableFooterMenu = false;
+            view.OptionsMenu.EnableGroupPanelMenu = false;
+
+            view.OptionsNavigation.EnterMoveNextColumn = true;
+
+            view.OptionsPrint.AutoWidth = false;
+            view.OptionsPrint.PrintFooter = false;
+            view.OptionsPrint.PrintGroupFooter = false;
+
+            view.OptionsView.ShowViewCaption = true;
+            view.OptionsView.ShowAutoFilterRow = true;
+            view.OptionsView.ShowGroupPanel = false;
+            view.OptionsView.ColumnAutoWidth = false;
+            view.OptionsView.RowAutoHeight = true;
+            view.OptionsView.HeaderFilterButtonShowMode = FilterButtonShowMode.Button;
+            view.FocusRectStyle = DrawFocusRectStyle.RowFocus;
+
+
+            var idColumn = new MyGridColumn();
+            idColumn.Caption = "Id";
+            idColumn.FieldName = "Id";
+            idColumn.OptionsColumn.AllowEdit = false;
+            idColumn.OptionsColumn.ShowInCustomizationForm = false;
+            view.Columns.Add(idColumn);
+
+            var kodColumn = new MyGridColumn();
+            kodColumn.Caption = "Kod";
+            kodColumn.FieldName = "Kod";
+            kodColumn.Visible = true;
+            kodColumn.OptionsColumn.AllowEdit = false;
+            kodColumn.AppearanceCell.TextOptions.HAlignment = HorzAlignment.Center;
+            kodColumn.AppearanceCell.Options.UseTextOptions = true;
+            view.Columns.Add(kodColumn);
+
+            return view;
+        }
+        protected override void RegisterAvailableViewsCore(InfoCollection collection)
+        {
+            base.RegisterAvailableViewsCore(collection);
+            collection.Add(new MyGridInfoRegistrator());
+        }
+        private class MyGridInfoRegistrator : GridInfoRegistrator
+        {
+            public override string ViewName => "MyGridView";
+            public override BaseView CreateView(GridControl grid) => new MyGridView(grid);
+        }
+    }
+    public class MyGridView : GridView, IStatusBarKisayol
+    {
+        #region Properties
+        public string StatusBarKisayol { get; set; }
+        public string StatusBarKisayolAciklama { get; set; }
+        public string StatusBarAciklama { get; set; }
+        #endregion
+        public MyGridView() { }
+        public MyGridView(GridControl ownerGrid) : base(ownerGrid) { 
+         
+             
+             Appearance.Row.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Default;
+             Appearance.Row.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            Appearance.Row.Options.UseTextOptions = true;
+            OptionsMenu.ShowGroupSortSummaryItems = true;
+            OptionsMenu.ShowSummaryItemMode = DefaultBoolean.True;
+            OptionsMenu.ShowFooterItem = true;
+            OptionsView.ShowFooter = true;
+            OptionsView.BestFitMode = GridBestFitMode.Fast;
+            VertScrollVisibility = ScrollVisibility.Auto;
+            
+
+
+
+        }
+        protected override void OnColumnChangedCore(GridColumn column)
+        {
+            base.OnColumnChangedCore(column);
+
+            if (column.ColumnEdit == null) return;
+            if (column.ColumnEdit.GetType() == typeof(RepositoryItemDateEdit))
+            {
+                column.AppearanceCell.TextOptions.HAlignment = HorzAlignment.Center;
+                ((RepositoryItemDateEdit)column.ColumnEdit).Mask.MaskType = MaskType.DateTimeAdvancingCaret;
+            }
+        }
+        protected override GridColumnCollection CreateColumnCollection()
+        {
+            return new MyGridColumnCollection(this);
+        }
+
+        private class MyGridColumnCollection : GridColumnCollection
+        {
+            public MyGridColumnCollection(ColumnView view) : base(view) { }
+            protected override GridColumn CreateColumn()
+            {
+                var column = new MyGridColumn();
+                column.OptionsColumn.AllowEdit = false;
+                return column;
+            }
+
+        }
+    }
+    public class MyGridColumn : GridColumn, IStatusBarKisayol
+    {
+        #region Properties
+        public string StatusBarKisayol { get; set; }
+        public string StatusBarKisayolAciklama { get; set; }
+        public string StatusBarAciklama { get; set; }
+        #endregion
+    }
+
+}
